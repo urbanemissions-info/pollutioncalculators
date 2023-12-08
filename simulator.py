e22c1a9d341fdfa5c2b826a12daa350a25b252a2
Sai Krishna <saikrishnadammalapati@gmail.com>
2023-12-08 22:45:05 +0530

    UPDATE SIMULATOR -- Added scientific computing

--- simulator.py
+++ simulator.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 import streamlit as st
 st.set_page_config(layout="wide", page_title="AQI Simulator")
 
@@ -46,7 +48,7 @@ tab1, tab2, tab3, tab4 = st.tabs(["V1", "V2", "V3", "V4"])
 
 #*************** TAB 1 - V1 ***************#
 with tab1:
-    num_zones = st.number_input("Number of Zones in the City: ", value=10)    
+    num_zones = st.number_input("Number of Zones in the City: ", value=5)    
     col1, col2, col3, col4, col5 = st.columns(5)
 
     variables_dict = dict()
@@ -60,7 +62,7 @@ with tab1:
 
     with col2:
         st.write("### Zone.pop (mil)")
-        default_values = [1.1, 1.3, 1.9, 2.0, 2.1, 1.9, 2.5, 0.5, 1.0, 0.1]
+        default_values = [1.3, 1.3, 1.3, 1.3, 1.3, 1.3, 1.3, 1.3, 1.3, 1.3]
         var_name = f"pop_{i}"
         for i in range(num_zones):
             keyplace = default_values_zones[i]+'_pop'
@@ -79,7 +81,7 @@ with tab1:
             
     with col3:
         st.write("### Avg.Conc μg/m3")
-        default_values = [55, 65, 75, 85, 95, 100, 110, 35, 65, 20]
+        default_values = [60, 60, 60, 60, 60, 60, 60, 60, 60, 60]
         var_name = f"conc_{i}"
         for i in range(num_zones):
              keyplace = default_values_zones[i]+'_conc'
@@ -96,7 +98,7 @@ with tab1:
             sumprod = sumprod+prod
         
         pop_weighted_conc = sumprod/total_pop
-        st.write('Population weighted concentration: {} μg/m3'.format(round(pop_weighted_conc,1)))
+        st.write('#### :green[Population weighted concentration: {} μg/m3]'.format(round(pop_weighted_conc,1)))
 
     
     with col4:
@@ -108,6 +110,7 @@ with tab1:
             #     st.session_state[keyplace] = default_values[i] #Default val
             variables_dict[var_name] = st.slider("Avg.Conc μg/m3",
                         label_visibility='collapsed', key=keyplace)
+            
             
     with col5:
         st.write("### New zone average")
@@ -125,8 +128,40 @@ with tab1:
             sumprod = sumprod+prod
         
         new_pop_weighted_conc = sumprod/total_pop
-        st.write('New Population weighted concentration:  {} μg/m3'.format(round(new_pop_weighted_conc,1)))
+        st.write('#### :green[New Population weighted concentration:  {} μg/m3]'.format(round(new_pop_weighted_conc,1)))
 
+    with col4:
         net_reduction = round(100*(pop_weighted_conc-new_pop_weighted_conc)/pop_weighted_conc, 1)
         st.write('#### :green[Net reduction:  {} %]'.format(round(net_reduction,1)))
+
+
+#*************** TAB 3 - V3 ***************#
+with tab3:
+    zone_concentrations_df = pd.read_csv('zone_concentrations_default.csv')
+    sourceapportionment_df = pd.read_csv('sourceapportionment_default.csv')
+    reduction_sourcewise_df = pd.read_csv('reduction_sourcewise.csv')
+
+    sourceapportionment_array = np.array(sourceapportionment_df.iloc[:,1:].values)
+    zone_concentrations_array = np.array(zone_concentrations_df.iloc[:,1:2].values)
+    zone_populations_array = np.array(zone_concentrations_df.iloc[:,2:].values)
+    pop_weighted_conc = np.dot(zone_concentrations_array.T,zone_populations_array)/np.sum(zone_populations_array)
+    
+    reduction_sourcewise_array = np.array(reduction_sourcewise_df.iloc[:,1:].values)
+    reduction_sourcewise_array = 1- reduction_sourcewise_array
+
+    source_concentrations_old = sourceapportionment_array*(zone_concentrations_array.T)
+    source_concentrations_new = source_concentrations_old*reduction_sourcewise_array
+
+    zone_concentrations_avg_new = np.sum(source_concentrations_new, axis=0)
+    sourceapportionment_new = source_concentrations_new/zone_concentrations_avg_new
+
+    source_pmsa_old = np.dot(source_concentrations_old,zone_populations_array)/np.sum(zone_populations_array)
+    source_pmsa_new = np.dot(source_concentrations_new,zone_populations_array)/np.sum(zone_populations_array)
+
+    
+    pop_weighted_conc_new = np.dot(zone_concentrations_avg_new.T,zone_populations_array)/np.sum(zone_populations_array)
+
+    net_reduction = 100*(pop_weighted_conc-pop_weighted_conc_new)/pop_weighted_conc
+
+    print(net_reduction)
 
